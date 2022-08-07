79ca24cd3619b88367d3a70c3f9fa34dcff2c342
Pratik Narkhede <pratiknarkhede646@gmail.com>
2022-08-07 12:03:59 +0530

    leetcode121 best time to sell stocks

---
+++ src/twopointers/BestTimeToSellStock.java
@@ -0,0 +1,29 @@
+package twopointers;
+
+public class BestTimeToSellStock {
+
+	public static void main(String[] args) {
+		int[] arr = {2,1,2,1,0,1,2};
+		int profit = maxProfit(arr);
+		System.out.println(profit);
+	}
+
+	private static int maxProfit(int[] arr) {
+		int buy = 0, sell = 1;
+		int  maxProfit = 0;
+		while (buy<arr.length && sell <arr.length) {
+			if (arr[buy] > arr[sell]) {
+				buy=sell;
+				sell++;
+			} else {
+				if ((arr[sell] - arr[buy]) > maxProfit) {
+					maxProfit = arr[sell] - arr[buy];
+				}
+			sell++;	
+			}
+		}
+
+		return maxProfit;
+	}
+
+}
